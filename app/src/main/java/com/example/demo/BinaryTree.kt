9cc42200865c9d2ada352152957011c654f52e22
luzheng <luzheng611@gmail.com>
2021-06-05 22:52:13 +0800

    二叉树最大深度

--- app/src/main/java/com/example/demo/BinaryTree.kt
+++ app/src/main/java/com/example/demo/BinaryTree.kt
@@ -1,5 +1,7 @@
 package com.example.demo
 
+import java.util.*
+
 fun main() {
 
 }
@@ -9,8 +11,8 @@ fun main() {
  * 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点
  */
 fun mergeTrees(root1: TreeNode?, root2: TreeNode?): TreeNode? {
-    root1 ?: return  root2
-    root2 ?: return  root1
+    root1 ?: return root2
+    root2 ?: return root1
     val ans = TreeNode(root1.value + root2.value)
     ans.left = mergeTrees(root1.left, root2.left)
     ans.right = mergeTrees(root1.right, root2.right)
@@ -28,4 +30,25 @@ fun mirrorTree(root: TreeNode?): TreeNode? {
     mirrorTree(root.right)
     mirrorTree(root.left)
     return root
+}
+
+/**
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ */
+fun maxDepth(root: TreeNode?): Int {
+    root ?: return 0
+    val queue = LinkedList<TreeNode>()
+    queue.offer(root)
+    var ans = 0
+    while (!queue.isEmpty()) {
+        val size = queue.size
+        if(size > 0 ) ans++
+        for (i in 0 until size) {
+            val cur = queue.poll()
+            cur ?: return ans
+            if (cur.left != null) queue.offer(cur.left)
+            if (cur.right != null) queue.offer(cur.right)
+        }
+    }
+    return ans
 }
